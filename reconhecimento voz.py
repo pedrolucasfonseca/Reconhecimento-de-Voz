c827686e18a5197f4a12823e9883f80692be3525
Pedro Lucas Fonseca Vieira <pedrolucasfonseca98@gmail.com>
2025-09-07 17:24:49 -0300

    Update reconhecimento voz.py

--- reconhecimento voz.py
+++ reconhecimento voz.py
@@ -4,12 +4,13 @@ def ouvir_microfone(tempo=10):
     r = sr.Recognizer()
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source, duration=1)
-        audio = r.record(source, duration=tempo)
         print("Escutando...")
+        audio = r.record(source, duration=tempo)
         try:
             texto = r.recognize_google(audio, language='pt-BR')
             print(f"Transcrição: {texto}")
         except sr.UnknownValueError:
             print("Não foi possível transcrever o que foi falado.")
+
 
 ouvir_microfone(10)
